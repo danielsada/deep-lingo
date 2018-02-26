bc5fe35f897a3cafb740ebf351939781d0630fc2
Daniel Sada Caraveo <danielsadac@gmail.com>
2018-02-26 14:39:41 -0600

    Added deep-lingo-2 which is a copy of the original deep-lingo-1

---
+++ deep-lingo-2/Tests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+namespace DeepLingo {
+
+	public class ScannerTest {
+
+		public void RunTests () {
+			TestFile ("./example-programs/arrays.deep", "Arrays");
+			TestFile("./example-programs/binary.deep", "Binary");
+			TestFile("./example-programs/literals.deep", "Literals");
+			TestFile("./example-programs/next_day.deep", "Next Day");
+			TestFile("./example-programs/palindrome.deep", "Palindrome");
+			TestFile("./example-programs/ultimate.deep", "Ultimate");
+		}
+
+		public void TestFile (string inputFile, string name) {
+			Console.WriteLine($"Running {name}, ==Sit tight==");
+			var input = File.ReadAllText(inputFile);
+			var count = 1;
+			foreach (var tok in new Scanner(input).Start()) {
+				if (tok.Category == TokenType.ILLEGAL_CHAR)
+					Console.WriteLine(String.Format("[{0}] {1} Arrojó illegal char", 
+                                                    count++, tok)
+                    );
+
+			}
+
+		}
+
+	}
+
+}
