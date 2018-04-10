390e554333f79cb661146bb5bcab3d24b3ab500c
Daniel Sada Caraveo <danielsadac@gmail.com>
2018-04-09 21:18:18 -0500

    More preparations for next phase

--- deep-lingo/NodeList.cs
+++ deep-lingo/NodeList.cs
@@ -20,33 +20,17 @@ namespace DeepLingo {
     class OperatorComp : Node { }
     class OperatorMath : Node { }
 
-    class StmtCall : Node { }
     class FunctionCall : Node { }
-
-    class RootFunctionCall : Node { }
-    class StmtIdOrFunCall : Node { }
-    class ExprIdOrFunCall : Node { }
-
     class StatementList : Node { };
     class Statement : Node { };
-    class ExpressionList : Node { };
     class ElseIfList : Node { };
     class ElseIf : Node { };
     class Else : Node { };
     class Literal : Node { }
     class Operator : Node { }
     class Return : Node { }
-
-    class IdOrFunCall : Node { }
-
     class Increment : Node { }
     class Decrement : Node { }
-    class FunCall : Node { }
-    //Some expressions
-
-    // More created afterwards
-    // Expression Unary elements
-
     class Positive : Node { }
 
     class Negative : Node { }
---
+++ deep-lingo/SymbolTable.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepLingo {
+
+    public class SymbolTable : IEnumerable<KeyValuePair<string, Type>> {
+
+        IDictionary<string, Type> data = new SortedDictionary<string, Type> ();
+
+        //-----------------------------------------------------------
+        public override string ToString () {
+            var sb = new StringBuilder ();
+            sb.Append ("Symbol Table\n");
+            sb.Append ("====================\n");
+            foreach (var entry in data) {
+                sb.Append (String.Format ("{0}: {1}\n",
+                    entry.Key,
+                    entry.Value));
+            }
+            sb.Append ("====================\n");
+            return sb.ToString ();
+        }
+
+        //-----------------------------------------------------------
+        public Type this [string key] {
+            get {
+                return data[key];
+            }
+            set {
+                data[key] = value;
+            }
+        }
+
+        //-----------------------------------------------------------
+        public bool Contains (string key) {
+            return data.ContainsKey (key);
+        }
+
+        //-----------------------------------------------------------
+        public IEnumerator<KeyValuePair<string, Type>> GetEnumerator () {
+            return data.GetEnumerator ();
+        }
+
+        //-----------------------------------------------------------
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator () {
+            throw new NotImplementedException ();
+        }
+    }
+}
